7a05d09708e02c7eaf93b1d1cdbf4a2cc2193812
JadeMin <bokmook01@gmail.com>
2023-01-13 03:46:18 +0900

    명령어 모음집? 구하는 코드 정리

--- src/interactions/all.js
+++ src/interactions/all.js
@@ -1,6 +1,7 @@
-import ctxmMessage from "./context_menu/message/*.jsx";
 import ctxmUser from "./context_menu/user/*.jsx";
+import ctxmMessage from "./context_menu/message/*.jsx";
 const slash_commands = require(`./slash/*.jsx`);
+
 let result = {
 	commands: {},
 	userCommands: {},
@@ -8,22 +9,20 @@ let result = {
 };
 
 
-const pushCommands = (callback, propName) => {
-	const [key, value] = [
-		Object.keys(callback.default)[0],
-		Object.values(callback.default)[0]
-	];
-	result[propName][key] = value;
+const pushCommands = (propName, callback, index) => {
+	if(propName === 'commands') {
+		const commandName = slash_commands.filenames[index].replace(/.*\/(.*)\.jsx$/gi, '$1');
+		result[propName][commandName] = callback.default;
+	} else {
+		const [key, value] = [
+			Object.keys(callback.default)[0],
+			Object.values(callback.default)[0]
+		];
+		result[propName][key] = value;
+	}
 };
-ctxmUser.forEach(callback => {
-	pushCommands(callback, 'userCommands');
-});
-ctxmMessage.forEach(callback => {
-	pushCommands(callback, 'messageCommands');
-});
-slash_commands.default.forEach((callback, index) => {
-	const commandName = slash_commands.filenames[index].replace(/.*\/(.*)\.jsx$/gi, '$1');
-	result.commands[commandName] = callback.default;
-});
+ctxmUser.forEach(c=> pushCommands(c, 'userCommands'));
+ctxmMessage.forEach(c=> pushCommands(c, 'messageCommands'));
+slash_commands.default.forEach((c, i)=> pushCommands('commands', c, i));
 
 export default result;
