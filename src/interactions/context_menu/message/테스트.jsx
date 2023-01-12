df881e0bd16c31728a643ae70bd924a7ca3e502f
JadeMin <bokmook01@gmail.com>
2023-01-13 03:41:56 +0900

    인자명 수정

--- src/interactions/context_menu/message/테스트.jsx
+++ src/interactions/context_menu/message/테스트.jsx
@@ -7,9 +7,9 @@ import {
 
 export default {
 	"테스트 메시지메뉴": () => {
-		return (interaction, cfg, ctx, targetMessage) => {
+		return (interaction, secret, ctx, targetMessage) => {
 			const messageUrl = `https://discord.com/channels/${interaction.guild_id ?? "@me"}/${targetMessage.channel_id}/${targetMessage.id}`;
-
+			
 			return (
 				<Message ephemeral>
 					{`[메시지](<${messageUrl}>)의 메시지 메뉴를 누르셨네요.`}
--- src/interactions/context_menu/user/테스트.jsx
+++ src/interactions/context_menu/user/테스트.jsx
@@ -7,7 +7,7 @@ import {
 
 export default {
 	"테스트 유저메뉴": () => {
-		return async (interaction, cfg, ctx, targetUser) => {
+		return (interaction, secret, ctx, targetUser) => {
 			return (
 				<Message ephemeral>
 					{`<@${interaction.member.user.id}>님이 <@${targetUser.id}>님의 테스트 유저 메뉴를 눌렀습니다.`}
