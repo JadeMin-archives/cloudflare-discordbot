8abb1c4216a18e4075a0bf6cad72480fb696a3b9
JadeMin <>
2023-01-21 15:24:10 +0900

    예제 추가

--- src/interactions/slash/계산.jsx
+++ src/interactions/slash/계산.jsx
@@ -57,6 +57,56 @@ export default {
 					}
 				</Message>
 			);
+		},
+		"곱하기": () => {
+			// 명령어 설명
+			useDescription("두 숫자의 곱셈 결과값을 구합니다.");
+
+			// 명령어에 사용될 명령어 옵션
+			const argv_n1 = useInteger("숫자1", "곱셈에 사용될 첫번째 숫자값입니다.", {
+				required: true,
+				min: 1, max: 9999,
+			});
+			const argv_n2 = useInteger("숫자2", "곱셈에 사용될 두번째 숫자값입니다.", {
+				required: true,
+				min: 1, max: 9999,
+			});
+			
+
+			// 슬래시 명령어 사용 시 반환할 컴포넌트
+			return (interaction, workerConfig, workerContext) => (
+				<Message>
+					{
+						// 숫자 1과 숫자 2를 곱합니다.
+						argv_n1 * argv_n2
+					}
+				</Message>
+			);
+		},
+		"나누기": () => {
+			// 명령어 설명
+			useDescription("두 숫자의 나눗셈 결과값을 구합니다.");
+
+			// 명령어에 사용될 명령어 옵션
+			const argv_n1 = useInteger("숫자1", "나눗셈에 사용될 첫번째 숫자값입니다.", {
+				required: true,
+				min: 1, max: 9999,
+			});
+			const argv_n2 = useInteger("숫자2", "나눗셈에 사용될 두번째 숫자값입니다.", {
+				required: true,
+				min: 1, max: 9999,
+			});
+			
+
+			// 슬래시 명령어 사용 시 반환할 컴포넌트
+			return (interaction, workerConfig, workerContext) => (
+				<Message>
+					{
+						// 숫자 1과 숫자 2를 나눕니다.
+						argv_n1 / argv_n2
+					}
+				</Message>
+			);
 		}
 	}
 };
